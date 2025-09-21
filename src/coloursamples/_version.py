72d283610c723cfb34207a07d78c75e273d9b218
Jack McPherson <jack@Jacks-Mac-mini.local>
2025-09-21 13:10:23 +1000

    Fix version and sort imports

--- src/coloursamples/_version.py
+++ src/coloursamples/_version.py
@@ -27,7 +27,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.4.dev5+gf330e0e6d.d20250921'
-__version_tuple__ = version_tuple = (0, 1, 4, 'dev5', 'gf330e0e6d.d20250921')
+__version__ = version = "0.1.4.dev6+g443c83578.d20250921"
+__version_tuple__ = version_tuple = (0, 1, 4, "dev6", "g443c83578.d20250921")
 
 __commit_id__ = commit_id = None
