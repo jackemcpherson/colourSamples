021e43b740410080e8d5bb6ae51091d7d2f390d2
Jack McPherson <jack@Jacks-Mac-mini.local>
2025-09-21 13:15:01 +1000

    Auto-update version file for release

--- src/coloursamples/_version.py
+++ src/coloursamples/_version.py
@@ -12,7 +12,8 @@ __all__ = [
 
 TYPE_CHECKING = False
 if TYPE_CHECKING:
-    from typing import Tuple, Union
+    from typing import Tuple
+    from typing import Union
 
     VERSION_TUPLE = Tuple[Union[int, str], ...]
     COMMIT_ID = Union[str, None]
@@ -27,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.1.4.dev9+gb417fca79.d20250921"
-__version_tuple__ = version_tuple = (0, 1, 4, "dev9", "gb417fca79.d20250921")
+__version__ = version = '0.1.4.dev10+g94cf0cce8.d20250921'
+__version_tuple__ = version_tuple = (0, 1, 4, 'dev10', 'g94cf0cce8.d20250921')
 
 __commit_id__ = commit_id = None
