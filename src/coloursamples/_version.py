443c835783e4fac5c6efb7f029af41c66f3e967a
Jack McPherson <jack@Jacks-Mac-mini.local>
2025-09-21 13:09:22 +1000

    Fix version and sort imports

--- src/coloursamples/_version.py
+++ src/coloursamples/_version.py
@@ -12,8 +12,7 @@ __all__ = [
 
 TYPE_CHECKING = False
 if TYPE_CHECKING:
-    from typing import Tuple
-    from typing import Union
+    from typing import Tuple, Union
 
     VERSION_TUPLE = Tuple[Union[int, str], ...]
     COMMIT_ID = Union[str, None]
@@ -28,7 +27,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.4.dev4+ge6781bac5.d20250921'
-__version_tuple__ = version_tuple = (0, 1, 4, 'dev4', 'ge6781bac5.d20250921')
+__version__ = version = '0.1.4.dev5+gf330e0e6d.d20250921'
+__version_tuple__ = version_tuple = (0, 1, 4, 'dev5', 'gf330e0e6d.d20250921')
 
 __commit_id__ = commit_id = None
